7e41c4c58142d3c1853e8d3a4d54f42d6dbf08e0
TranTam <tamvaa1306@gmail.com>
2024-04-03 11:42:10 +0700

    init tiptap editor

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <editor-content :editor="editor"></editor-content>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Editor, EditorContent } from "@tiptap/vue-2";
+import StarterKit from "@tiptap/starter-kit";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    EditorContent,
+  },
+  data() {
+    return {
+      editor: null,
+    };
+  },
+  mounted() {
+    this.editor = new Editor({
+      content: "<p>I'm running Tiptap with Vue.js. 🎉</p>",
+      extensions: [StarterKit],
+    });
+  },
+
+  beforeDestroy() {
+    this.editor.destroy();
+  },
+};
 </script>
 
 <style lang="scss">
